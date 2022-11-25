86819a98a2950d9f97d330a51433fb5e483488ed
Mikky <meekyberry6@gmail.com>
2022-11-25 02:12:13 +0100

    Code update

---
+++ .history/src/App_20221124201043.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Route, Routes } from "react-router-dom";
+import { Home, Navigation, Booking, FallBack, Error } from "./components";
+import { Login, User, Logout, SignUp } from "./profile";
+import RequireAuth from "./profile/context/require";
+import { AuthProvider } from "./profile/context/auth";
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary FallbackComponent={FallBack}>
+        <Navigation />
+        <AuthProvider>
+          <Routes>
+            <Route path="/" >
+              <Route index element={<Home />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path=":signUp" element={<SignUp /> } />
+            <Route
+              path="/user"
+              element={
+                <RequireAuth>
+                  <User />
+                </RequireAuth>
+              }
+            />
+            <Route path="/logout" element={<Logout />} />
+            {/* <Route path="/signUp" element={<SignUp />} /> */}
+            <Route path="/booking" element={<Booking />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </AuthProvider>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/error_20221124215741.jsx
@@ -0,0 +1,25 @@
+// ERROR COMPONENT:   This component is for the error message in the app when the user routes to a page that doesn't exist
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/error.css";
+import { Helmet } from "react-helmet";
+
+const Error = () => {
+  return (
+    <div className="error">
+      <Helmet>
+        <title>404 Error</title>
+      </Helmet>
+      <h1 className="error__title">404</h1>
+      <h2 className="error__heading">Page not found</h2>
+      <p className="error__message">
+        The page you are looking for might have been removed, had its name
+        changed, or is temporarily unavailable.
+      </p>
+      <Link to="/" className="error__back--link">Go to Home</Link>
+    </div>
+  );
+};
+
+export default Error;
